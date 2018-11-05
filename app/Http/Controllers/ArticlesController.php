b67b78c07cb29f53e079c1e6119ec8f1ba4a6df0
yang3529369110 <1443261811@qq.com>
2018-11-05 14:38:59 +0800

    文章管理

--- app/Http/Controllers/ArticlesController.php
+++ app/Http/Controllers/ArticlesController.php
@@ -2,7 +2,9 @@
 
 namespace App\Http\Controllers;
 
-use App\Http\Requests;
+use App\Tag;
+use App\Article;
+use App\Category;
 use Illuminate\Http\Request;
 
 class ArticlesController extends Controller
@@ -18,10 +20,72 @@ class ArticlesController extends Controller
      */
     public function index()
     {
-        return view('article.index');
+        $articles = Article::orderBy('created_at', 'desc')->paginate(20);
+        return view('article.index', compact('articles'));
     }
+
+    /**
+     * 创建新文章
+     *
+     * @return mixed
+     */
     public function create()
     {
-        return view('article.create');
+        $tags = Tag::all();
+        $categories = Category::all();
+        return view('article.create', compact('tags', 'categories'));
+    }
+
+    /**
+     * 存储文章内容
+     *
+     * @param Request $request
+     * @return \Illuminate\Http\JsonResponse
+     */
+    public function store(Request $request)
+    {
+        $this->validator($request);
+        $input = $request->all() + ['user_id' => 1, 'last_user_id' => 1];
+        $article = new Article();
+        $result = $article->fill($input)->save();
+        $result = $article->tags()->sync($input['tags']);
+
+        return response()->json(true);
+    }
+
+    public function delete(Request $request, $id)
+    {
+        $result = Article::destroy($id);
+        return response()->json($result);
+    }
+
+    /**
+     * 验证文章字段
+     *
+     * @param Request $request
+     * @return array
+     */
+    protected function validator(Request $request)
+    {
+        $rules = [
+            'title'       => 'bail|required|min:3',
+            'subtitle'    => 'bail|required|min:2',
+            'content'     => 'bail|required',
+            'category_id' => 'bail|required',
+            'tags'        => 'bail|required',
+            'page_image'  => 'bail|required',
+        ];
+        $messages =  [
+            'title.required'       => '标题必填',
+            'title.min'            => '标题至少三个字符',
+            'subtitle.required'    => '副标题必填',
+            'subtitle.min'         => '副标题至少三个字符',
+            'content.required'     => '文章内容必填',
+            'category_id.required' => '文章分类必填',
+            'tags.required'        => '文章标签必填',
+            'page_image.required'  => '文章封面必填',
+        ];
+
+        return $this->validate($request, $rules, $messages);
     }
 }
