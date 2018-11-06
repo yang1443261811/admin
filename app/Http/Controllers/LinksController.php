97560746cc2c902f523812c184a9ff5ca46538cd
yang1443261811 <1443261811@qq.com>
2018-11-06 17:23:10 +0800

    1

--- app/Http/Controllers/LinksController.php
+++ app/Http/Controllers/LinksController.php
@@ -2,7 +2,7 @@
 
 namespace App\Http\Controllers;
 
-use App\Http\Requests;
+use App\Link;
 use Illuminate\Http\Request;
 
 class LinksController extends Controller
@@ -18,11 +18,60 @@ class LinksController extends Controller
      */
     public function index()
     {
-        return view('link.index');
+        $links = Link::orderBy('created_at', 'desc')
+            ->paginate(20);
+        return view('link.index', ['links' => $links]);
     }
 
-    public function create()
+    public function create(Request $request)
     {
-        return view('link.create');
+        if ($request->method() == 'GET') {
+            return view('link.create');
+        }
+
+        $this->validator($request);
+        $result = (new Link)->fill($request->all())->save();
+        return response()->json($result);
+    }
+
+    public function edit($id)
+    {
+        return view('link.update', ['link' =>  Link::find($id)]);
+    }
+
+    public function update(Request $request, $id)
+    {
+        $this->validator($request);
+        $model = Link::find($id);
+        $res = $model->fill($request->all())->save();
+        return response()->json($res);
+    }
+
+    public function delete($id)
+    {
+        $result = Link::destroy($id);
+        return response()->json($result);
+    }
+
+    /**
+     * 验证文章字段
+     *
+     * @param Request $request
+     * @return array
+     */
+    protected function validator(Request $request)
+    {
+        $rules = [
+            'name'  => 'bail|required',
+            'link'  => 'bail|required',
+            'image' => 'bail|required',
+        ];
+        $messages =  [
+            'name.required'  => '名字必填',
+            'link.required'  => '链接必填',
+            'image.required' => '图片不能为空',
+        ];
+
+        return $this->validate($request, $rules, $messages);
     }
 }
