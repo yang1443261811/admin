53a26d9c49feb9ebc692a74834f9b70ca542eb56
yqm <yangqm@iclockwork.com>
2018-11-07 11:43:21 +0800

    显示服务器信息

--- app/Http/Controllers/SystemController.php
+++ app/Http/Controllers/SystemController.php
@@ -24,6 +24,23 @@ class SystemController extends Controller
      */
     public function index()
     {
-        return view('system.index');
+        $pdo     = \DB::connection()->getPdo();
+
+        $version = $pdo->query('select version()')->fetchColumn();
+
+        $data = [
+            'server'        => $_SERVER['SERVER_SOFTWARE'],
+            'http_host'     => $_SERVER['HTTP_HOST'],
+            'remote_host'   => isset($_SERVER['REMOTE_HOST']) ? $_SERVER['REMOTE_HOST'] : $_SERVER['REMOTE_ADDR'],
+            'user_agent'    => $_SERVER['HTTP_USER_AGENT'],
+            'php'           => phpversion(),
+            'sapi_name'     => php_sapi_name(),
+            'extensions'    => implode(", ", get_loaded_extensions()),
+            'db_connection' => isset($_SERVER['DB_CONNECTION']) ? $_SERVER['DB_CONNECTION'] : 'Secret',
+            'db_database'   => isset($_SERVER['DB_DATABASE']) ? $_SERVER['DB_DATABASE'] : 'Secret',
+            'db_version'    => $version,
+        ];
+
+        return view('system.index', $data);
     }
 }
